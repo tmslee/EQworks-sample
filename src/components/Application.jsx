f74d008b6e48c00002e8079f50b38731a86d2f19
Sang Hyup Lee <tms.lee9@gmail.com>
2021-04-03 21:44:33 -0400

    initialized sample work boiler plate

---
+++ src/components/Application.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+
+import axios from 'axios';
+
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Application.scss'
+
+export default function Application() {
+
+
+  return (
+    <>
+    </>
+  );
+};
---
+++ src/stories/index.stories.jsx
@@ -0,0 +1,32 @@
+import React, {} from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import "../index.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// storiesOf("ChessNavBar", module)
+//   .add("not logged in", () => 
+//     <ChessNavbar
+//       currentUser={null}
+//     />)
+
+//   .add("logged in", () => 
+//     <ChessNavbar
+//       currentUser={"user1"}
+//     />)
+
+
+// storiesOf("HomeMenu", module)
+//   .add("default", () => 
+//     <HomeMenu
+//       currentUser={null}
+//     />)
+
+
+// storiesOf("GameAccept", module)
+//   .add("default", () => 
+//     <GameAccept
+//       currentUser={null}
+//     />)
