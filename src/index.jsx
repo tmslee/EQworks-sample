2178fd9ee9c2586797dcea53e24fccefab75fbc4
Sang Hyup Lee <tms.lee9@gmail.com>
2021-04-05 10:50:04 -0400

    cors fix

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,10 @@ import "./index.scss";
 
 import Application from "./components/Application";
 
-// if (process.env.REACT_APP_API_BASE_URL) {
-//   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
-// }
-axios.defaults.baseURL = '/api';
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 
 // ReactDOM.render(<Application />);
 ReactDOM.render(
